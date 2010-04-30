53be7faac6b96772bd1d5166a232c17c6f1a29ec
Mike Turquette <mturquette@ti.com>
2010-04-30 15:40:59 -0500

    OMAP3630: PM: enable DVFS on Zoom3 & add PM config
    This patch uses the new 3630 OPP tables in the Zoom3 board file.  With
    this change cpufreq now works on Zoom3.  For convenience this patch also
    introduces omap_zoom3_pm_defconfig which has all the PM options enabled
    to allow for DVFS via cpufreq.
    
    Change-Id: I7c824d983ec3b6b43b42efe3db2cf61991a2885c
    Signed-off-by: Mike Turquette <mturquette@ti.com>

--- arch/arm/mach-omap2/board-zoom3.c
+++ arch/arm/mach-omap2/board-zoom3.c
@@ -23,6 +23,7 @@
 
 #include "mux.h"
 #include "sdram-hynix-h8mbx00u0mer-0em.h"
+#include "omap3-opp.h"
 
 static void __init omap_zoom_map_io(void)
 {
@@ -39,7 +40,9 @@ static void __init omap_zoom_init_irq(void)
 	omap_board_config_size = ARRAY_SIZE(zoom_config);
 	omap2_init_common_hw(h8mbx00u0mer0em_sdrc_params,
 			     h8mbx00u0mer0em_sdrc_params,
-			     NULL, NULL, NULL);
+			     omap3630_mpu_rate_table,
+			     omap3630_dsp_rate_table,
+			     omap3630_l3_rate_table);
 	omap_init_irq();
 	omap_gpio_init();
 }
